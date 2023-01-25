5a5bf4c14cacd531ed2d7902a903f9826992be1c
Interborn <dylansafra@icloud.com>
2023-01-25 11:58:07 -0700

    Set up React components and containers to organize sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from 'react';
+import styles from './style';
+import { Hero, Navbar } from './components';
+import { Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer } from './containers';
 
-const App = () => {
-  <div className='bh-primary w-full overflow-hidden'>
-    <h1>Hello World!</h1>
+const App = () => (
+  <div className='bg-primary w-full overflow-hidden'>
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer />
+      </div>
+    </div>
+  
   </div>
-}
+)
 
 export default App
---
+++ src/containers/index.js
@@ -0,0 +1,19 @@
+import Stats from './Stats';
+import Business from './Business';
+import Billing from './Billing';
+import CardDeal from './CardDeal';
+import Testimonials from './Testimonials';
+import Clients from './Clients';
+import CTA from './CTA';
+import Footer from './Footer';
+
+export {
+    Stats,
+    Business,
+    Billing,
+    CardDeal,
+    Testimonials,
+    Clients,
+    CTA,
+    Footer,
+};
